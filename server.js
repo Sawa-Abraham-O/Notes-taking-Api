1b1b46d8b103858967e625cffd63fb902e4868a1
Sawa Abraham <sawaabraham03@gmail.com>
2026-08-27 15:15:05 +0100

    added todo

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const env = require("dotenv").config()
 const app = express();
 const PORT = process.env.PORT || 3000
 
-let id_count = 1
 let notes = [];
-
+let id_count = 1;
 
 app.use(express.json());
 
@@ -21,6 +20,26 @@ app.get("/",(req,res)=>{
   res.json({message:"Notes taking api is running successfully"})
 })
 
+// TODO
+/*
+1. create a get request to fetch all notes
+GET /notes
+
+
+create a post request to create a note
+POST /notes
+2.
+
+create a get request to fetch a single notes
+GET /notes/:id3.
+
+create a update request to update a note
+UPDATE /notes/:id4.
+
+create a delete request to delete a note
+DELETE /notes/:id
+
+*/
 
 app.listen(PORT,()=>{
   console.log(`app is running on http://localhost:${PORT}`);
